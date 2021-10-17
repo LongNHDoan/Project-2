657da15bf4f019c5dfd755efc36c83becdda340b
LongNHDoan <91375990+LongNHDoan@users.noreply.github.com>
2021-10-18 02:14:58 +0700

    Add files via upload

--- mJavaScript.js
+++ mJavaScript.js
@@ -5,13 +5,13 @@ var vg_4 = "annualIndustryCo2AreaChart.vl.json";
 var mapButtn = document.getElementById("mapBtn");
 var lineButtn = document.getElementById("lineBtn");
 
-vegaEmbed("#world", vg_1).then(function (result) { }).catch(console.error);
-vegaEmbed("#industry1", vg_2, { theme: "urbaninstitute" }).then(function (result) { }).catch(console.error);
-vegaEmbed("#industry2", vg_4, { theme: "urbaninstitute" }).then(function (result) { }).catch(console.error);
+vegaEmbed("#world", vg_1, { "actions": false }).then(function (result) { }).catch(console.error);
+vegaEmbed("#industry1", vg_2, { theme: "urbaninstitute", "actions": false }).then(function (result) { }).catch(console.error);
+vegaEmbed("#industry2", vg_4, { theme: "urbaninstitute", "actions": false }).then(function (result) { }).catch(console.error);
 
 mapButtn.onclick = function () {
     vegaEmbed("#world", vg_1).then(function (result) { }).catch(console.error);
 };
 lineButtn.onclick = function () {
-    vegaEmbed("#world", vg_3, { theme: "urbaninstitute" }).then(function (result) { }).catch(console.error);
+    vegaEmbed("#world", vg_3, { theme: "urbaninstitute", "actions": false }).then(function (result) { }).catch(console.error);
 };
